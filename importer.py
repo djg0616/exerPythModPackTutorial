c00d9ab1792a22bd2658267d8b5055c41d9afbf8
djg0616 <danniejgreenlee@gmail.com>
2020-09-07 21:40:30 -0600

    Mod pack files added

---
+++ importer.py
@@ -0,0 +1,6 @@
+import sys 
+print (sys.path)
+import mod
+import fact
+print(r'C:\Users\danni\OneDrive\Desktop\Grad School  Semester 1\Computer Science\myProject\exerPythModPackTutorial')
+print(r'C:\Users\danni\OneDrive\Desktop\Grad School  Semester 1\Computer Science\myProject\exerPythModPackTutorial')
